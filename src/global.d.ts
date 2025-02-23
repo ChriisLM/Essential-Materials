71f966377370823a0bf06cd8b26eb1b600ae2a9d
ChriisLM <christianlezcano878@gmail.com>
2025-02-23 14:31:55 -0500

    feat: Added global types

---
+++ src/global.d.ts
@@ -0,0 +1,16 @@
+export type Fonts = "openSans" | "roboto" | "poppins";
+export type Sizes = "small" | "medium" | "large";
+export type FontWeights = "light" | "normal" | "medium" | "bold";
+export type Spacing = "none" | "small" | "medium" | "large";
+export type LineStyle = "solid" | "dashed" | "dotted";
+export type TextPosition = "start" | "center" | "end";
+export type IconPosition = "start" | "end";
+
+export type ColorsScheme =
+  | "default"
+  | "primary"
+  | "secondary"
+  | "success"
+  | "warning"
+  | "error"
+  | "info";
