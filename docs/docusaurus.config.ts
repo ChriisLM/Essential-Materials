202cd98f152a8d78a3aef8bdfe93cec6523566fc
ChriisLM <christianlezcano878@gmail.com>
2025-03-13 14:21:52 -0500

    fix: Update the link to the website documentation and change the incorrect link in the blog post.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -7,10 +7,7 @@ const config: Config = {
   tagline: '',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://essential-materials.netlify.app/',
   baseUrl: '/',
 
   organizationName: 'ChriisLM',
@@ -20,8 +17,8 @@ const config: Config = {
   onBrokenMarkdownLinks: 'warn',
 
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es'],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
